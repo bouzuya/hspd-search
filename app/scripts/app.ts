8e42a0eabdb30a9282a9fa32111cb85e8b13113e
bouzuya <m@bouzuya.net>
2015-01-20 23:24:40 +0900

    import HubotScriptService

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,7 +1,9 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 
 import controllers = require('./controllers/index');
+import services = require('./services/index');
 
 angular
 .module('app', [])
+.service('HubotScriptService', services.HubotScriptService)
 .controller('AppCtrl', controllers.AppCtrl);
---
+++ app/scripts/services/index.ts
@@ -0,0 +1,3 @@
+import hss = require('./hubot-script-service');
+
+export var HubotScriptService = hss.HubotScriptService;
