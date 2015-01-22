59c75956020d806a096aa7a893a998056a907750
bouzuya <m@bouzuya.net>
2015-01-22 23:38:42 +0900

    add dummy test

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/power-assert/power-assert.d.ts" />
+import assert = require('power-assert');
+
+describe('Main', function() {
+  it('works', function() {
+    assert(1 === 1);
+  });
+});
